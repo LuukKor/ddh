c9cd51f949120e3348937d6544a2008618db7189
Lukasz <l.korkus1997@gmail.com>
2024-02-16 13:38:18 +0100

    Remove check for next and prev page (is handle by material)

--- src/services/people.ts
+++ src/services/people.ts
@@ -40,8 +40,6 @@ export class PeopleService {
       return {
         page: page,
         perPage: PEOPLE_PER_PAGE,
-        hideNextPage: !response.next,
-        hidePrevPage: !response.previous,
         totalPage: Math.ceil(response.count / PEOPLE_PER_PAGE),
         list: response.results.map((x) => ({ name: x.name })),
       };
--- src/types/list.ts
+++ src/types/list.ts
@@ -2,7 +2,5 @@ export interface List<T> {
   page: number;
   totalPage: number;
   perPage: number;
-  hideNextPage: boolean;
-  hidePrevPage: boolean;
   list: T[];
 }
