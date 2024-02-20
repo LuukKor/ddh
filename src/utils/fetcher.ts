ce13996ee8e3fc8c46611b036f26927961e01f94
Lukasz <l.korkus1997@gmail.com>
2024-02-20 12:19:04 +0100

    ADd post method to fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,8 +1,17 @@
-export async function fetcher<T>(url: string): Promise<T> {
+type IAPIMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+
+export async function fetcher<T>(url: string, method: IAPIMethod = 'GET', body = {}): Promise<T> {
+  const headers: HeadersInit = {
+    accept: 'application/json',
+  }
+
+  if (method === 'POST') {
+    headers.method = 'POST';
+    headers.body = JSON.stringify(body);
+  }
+
   const result = await fetch(url, {
-    headers: {
-      accept: 'application/json',
-    },
+    headers: headers
   }).then((res) => res.json());
 
   return result;
