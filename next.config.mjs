b70e4dfcc48481c6017782e9d6af73db14d0c936
Lukasz <l.korkus1997@gmail.com>
2024-02-20 17:13:19 +0100

    If /people redirect to hom

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/people',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
